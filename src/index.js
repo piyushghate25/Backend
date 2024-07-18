d0b875f22ab9826463d8baf07a47fd610e0f4e96
piyushghate25 <piyushghate37@gmail.com>
2024-07-18 20:12:36 +0530

    connect to mango atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require ('dotenv').config({path: './env'})  if we use this line then we donot  need the dotenv.config code.  this can directly access the envoirmental variables
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+import { DB_NAME } from "./constants.js";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+
+
+
+/*
+import express from "express";
+
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", ()=>{
+            console.log("Error",Err);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=>{
+            console.log("Server is running on port", `${process.env.PORT}`);
+        })
+
+    }catch(error){
+        console.log("Error",Err);
+        throw err
+    }
+
+})() 
+*/
