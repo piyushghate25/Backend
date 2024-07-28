36a9aca5fd04b21fca493707efa84487baa764ee
piyushghate25 <piyushghate37@gmail.com>
2024-07-28 23:07:22 +0530

    .

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { asyncHandler } from "../utilities/asyncHandler.js";
+import { ApiError } from "../utilities/ApiError.js";
+import { User } from "../models/User.js";
+import jwt from "json-web-token";
+
+export  const verifyJWT = asyncHandler( async (req ,res, next) =>{
+
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer","")
+    
+        if(!token){
+            throw new ApiError(401, "Unautorized request")
+        }
+    
+        const decodedToken =  jwt.verify(token , process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select(" -password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401, " Invalid Acess Token")
+        }
+        req.user = user;
+        next()
+    
+    } catch (error) {
+        throw new ApiError(401 , error?.message || "Invalid Access")
+    }
+})
